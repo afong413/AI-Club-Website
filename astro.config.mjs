64e085e8ae818d011fa7337167b7251372406ae6
Aiden Fong <afong413@gmail.com>
2024-09-11 18:48:25 -0700

    Add Vercel.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,11 @@ import { defineConfig } from 'astro/config';
 
 import solidJs from '@astrojs/solid-js';
 
+import vercel from '@astrojs/vercel/serverless';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [solidJs()]
+  integrations: [solidJs()],
+  output: 'server',
+  adapter: vercel()
 });
